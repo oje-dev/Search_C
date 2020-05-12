ec6099c96905ec4317d93d583f1530a454f977ea
oje-dev <44790306+oje-dev@users.noreply.github.com>
2020-05-12 22:54:13 +0100

    Implemented iterative binary search function.

--- search.c
+++ search.c
@@ -13,20 +13,25 @@
  *
  * You have to implement this function.
  */
-int linearsearch(int x, int arr[], int n) {
-    if (n <= 0 || arr == NULL){
+int linearsearch(int x, int arr[], int n)
+{
+    if (n <= 0 || arr == NULL)
+    {
         errno = EINVAL;
         return -1;
-
-    } else{
-        for (int i = 0; i < n; ++i){
-            if (arr[i] == x){
+    }
+    else
+    {
+        for (int i = 0; i < n; ++i)
+        {
+            if (arr[i] == x)
+            {
                 return i;
             }
         }
         return -1;
     }
- }
+}
 
 /*
  * See search.h for the specification of this recursive linear search
@@ -34,21 +39,29 @@ int linearsearch(int x, int arr[], int n) {
  *
  * You have to implement this function.
  */
-int rlinearsearch(int x, int arr[], int n, int i) {
-    if (n <= 0 || arr == NULL){
+int rlinearsearch(int x, int arr[], int n, int i)
+{
+    if (n <= 0 || arr == NULL)
+    {
         errno = EINVAL;
         return -1;
 
-    } else if (i == n && arr[i] != x){
+    }
+    else if (i == n && arr[i] != x)
+    {
         return -1;
 
-    } else if (arr[i] == x){
+    }
+    else if (arr[i] == x)
+    {
         return i;
 
-    } else{
+    }
+    else
+    {
         return rlinearsearch(x, arr, n, i + 1);
     }
- }
+}
 
 /*
  * See search.h for the specification of this sentinel linear search
@@ -56,21 +69,29 @@ int rlinearsearch(int x, int arr[], int n, int i) {
  *
  * You have to implement this function.
  */
-int slinearsearch(int x, int arr[], int n) {
-    if (n <= 0 || arr == NULL){
+int slinearsearch(int x, int arr[], int n)
+{
+    if (n <= 0 || arr == NULL)
+    {
         errno = EINVAL;
         return -1;
-    } else {
+    }
+    else
+    {
         int last = arr[n - 1];
         arr[n - 1] = x;
         int i = 0;
-        while (arr[i] != x){
+        while (arr[i] != x)
+        {
             i++;
         }
         arr[n - 1] = last;
-        if ((i < n - 1) || (x == arr[n - 1])){
+        if ((i < n - 1) || (x == arr[n - 1]))
+        {
             return i;
-        } else{
+        }
+        else
+        {
             return -1;
         }
     }
@@ -82,8 +103,27 @@ int slinearsearch(int x, int arr[], int n) {
  *
  * You have to implement this function.
  */
-int binarysearch(int x, int arr[], int n) {
-    return -1;
+int binarysearch(int x, int arr[], int n)
+{
+    if (n <= 0 || arr == NULL)
+    {
+        errno = EINVAL;
+        return -1;
+    }
+    else
+    {
+        int low = 0; int high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == x) return mid;
+            if (arr[mid] < x) {
+                low = mid + 1;
+            }else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
 
 /*
@@ -92,6 +132,7 @@ int binarysearch(int x, int arr[], int n) {
  *
  * You have to implement this function.
  */
-int rbinarysearch(int x, int arr[], int li, int hi) {
+int rbinarysearch(int x, int arr[], int li, int hi)
+{
     return -1;
 }
