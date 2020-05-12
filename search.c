7ec71855f3ff07f8138633eb33fcce30fb18cd0d
oje-dev <44790306+oje-dev@users.noreply.github.com>
2020-05-12 16:32:25 +0100

    Implemented linear search function.

--- search.c
+++ search.c
@@ -1,57 +1,69 @@
-/* 
+/*
  * search.c - the implementation of search functions.
- * 
+ *
  * This is the file you edit.
  */
 #include <errno.h>
 #include "search.h"
+#include <stddef.h>
 
-/* 
- * See search.h for the specification of this iterative linear search 
+/*
+ * See search.h for the specification of this iterative linear search
  * function.
  *
  * You have to implement this function.
  */
-int linearsearch(int x, int arr[], int n) { 
+int linearsearch(int x, int arr[], int n) {
+    if (n <= 0 || arr == NULL){
+        errno = EINVAL;
+        return -1;
+
+    } else{
+        for (int i = 0; i < n; ++i){
+            if (arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
+    }
+ }
+
+/*
+ * See search.h for the specification of this recursive linear search
+ * function.
+ *
+ * You have to implement this function.
+ */
+int rlinearsearch(int x, int arr[], int n, int i) {
     return -1;
  }
- 
-/* 
- * See search.h for the specification of this recursive linear search 
- * function.
- *
- * You have to implement this function.
- */
-int rlinearsearch(int x, int arr[], int n, int i) { 
-    return -1;
- }
- 
-/* 
- * See search.h for the specification of this sentinel linear search 
- * function.
- *
- * You have to implement this function.
- */
-int slinearsearch(int x, int arr[], int n) { 
-    return -1; 
-}
 
-/* 
- * See search.h for the specification of this iterative binary search 
+/*
+ * See search.h for the specification of this sentinel linear search
  * function.
  *
  * You have to implement this function.
  */
-int binarysearch(int x, int arr[], int n) {     
+int slinearsearch(int x, int arr[], int n) {
     return -1;
 }
 
-/* 
- * See search.h for the specification of this recursive binary search 
+/*
+ * See search.h for the specification of this iterative binary search
  * function.
  *
  * You have to implement this function.
  */
-int rbinarysearch(int x, int arr[], int li, int hi) { 
+int binarysearch(int x, int arr[], int n) {
+    return -1;
+}
+
+/*
+ * See search.h for the specification of this recursive binary search
+ * function.
+ *
+ * You have to implement this function.
+ */
+int rbinarysearch(int x, int arr[], int li, int hi) {
     return -1;
 }
